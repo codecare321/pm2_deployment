7ccbc36c28b26e1187937b8e7d92273e2aa1ee96
rahemanali <sales@junkiescoder.com>
2024-12-27 15:34:30 +0530

    added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello, RahemanAli! PM2 deployment is successful!");
 });
+app.get("/yes", (req, res) => {
+  res.send("wow");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
